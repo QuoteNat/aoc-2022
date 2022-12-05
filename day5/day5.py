a07d6b887b22c5f40d35a4c7e58a2a9afbc7858b
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-12-05 11:24:50 -0600

    Fetch initial state strings

---
+++ day5/day5.py
@@ -0,0 +1,20 @@
+from state import State
+
+inputs = list()
+with open("example", "r") as file:
+    inputs = file.readlines()
+
+# input cleaning
+for i in range(len(inputs)):
+    inputs[i] = inputs[i].replace("\n", "")
+
+# for line in inputs:
+#     print(line)
+
+# Pass in initial state information to the state constructor
+init_state = list()
+for i in range(inputs.index("")):
+    print(inputs[i])
+    init_state.append(inputs[i])
+
+state = State(init_state)
---
+++ day5/state.py
@@ -0,0 +1,7 @@
+from collections import deque
+
+# Problem state class representing the current state of the problem
+class State:
+    # Take a problem input as an array of strings
+    def __init__(self, input):
+        print(input)
