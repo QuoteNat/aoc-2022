f5ae3c36744ad6da9cc6107e1cf4008d5f21ce27
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-12-07 10:52:16 -0600

    Read input line by line

---
+++ day7/src/main.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::path::Path;
+
+fn main() {
+    // Create path to input file
+    let path = Path::new("example");
+    let display = path.display();
+
+    // Open the path in read-only mode
+    let file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let reader = BufReader::new(file);
+    // Read the file line by line
+    for line in reader.lines() {
+        let line = match line {
+            Err(why) => panic!("couldn't read line {}: {}", display, why),
+            Ok(line) => line,
+        };
+
+        println!("{}", line);
+    }
+}
